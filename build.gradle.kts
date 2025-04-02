54cad28dae44887410a477f98fe5b926894a9938
Eziocdl <eziocdl@gmail.com>
2025-04-02 20:21:52 -0300

    fix: Corrige erro de conexão com o banco de dados na classe DataBaseConnection
    Este commit corrige um erro que impedia a classe DataBaseConnection de estabelecer uma conexão bem-sucedida com o banco de dados.
    
    As seguintes alterações foram feitas:
    
    * Corrigido um problema com as configurações de conexão que causava falha na conexão.
    * Adicionado tratamento de exceção para lidar com erros de conexão.
    
    Este commit garante que a classe DataBaseConnection possa se conectar ao banco de dados sem problemas.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,8 @@ repositories {
 }
 
 dependencies {
+    implementation ("org.postgresql:postgresql:42.7.2")
+    implementation("org.postgresql:postgresql")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
